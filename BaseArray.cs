e234080ef81a24aecdddc850dd96a3803435ebec
makarovivan001 <157681853+makarovivan001@users.noreply.github.com>
2024-03-07 12:48:54 +0300

    Add files via upload

---
+++ BaseArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace HomeWork_Makarov_Ivan_number_3._1
+{
+	public abstract class BaseArray<T> : IBaseArray
+	{
+		protected ValueProvider<T> generator;
+		public BaseArray(bool personGet,ValueProvider<T> inputgenerator)
+		{
+			generator = inputgenerator;
+
+            Create(personGet);
+            
+		}
+		public void Create(bool personGet)
+		{
+			if (personGet)
+			{
+				GetfromPerson();
+			}
+			else
+			{
+				GetfromRandom();
+			}
+		}
+		protected abstract void GetfromPerson();
+        protected abstract void GetfromRandom();
+		public abstract void Print();
+        
+
+
+    }
+}
+
