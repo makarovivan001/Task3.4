e193731d526380d34eba4cb4d38124fb45271e5a
makarovivan001 <157681853+makarovivan001@users.noreply.github.com>
2024-03-07 12:54:03 +0300

    Add files via upload

---
+++ IBaseArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HomeWork_Makarov_Ivan_number_3._1
+{
+    interface IBaseArray : IPrinter
+    {
+
+        void Create(bool personGet);
+       
+
+        
+    }
+    
+
+
+       
+    
+	
+		
+	
+    
+}
+
